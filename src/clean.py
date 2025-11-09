a73bd12d7ab09dd2d1dc4cec36e45b26599a7ce2
Ritil-Ranjan <160457677+Ritil-Ranjan@users.noreply.github.com>
2025-11-09 18:54:04 +0530

    Add language detection and translation functions

--- src/clean.py
+++ src/clean.py
@@ -1,7 +1,18 @@
 # src/clean.py
+!pip install -U transformers
+!pip install googletrans==3.1.0a0
 from typing import List
 import re
 import emoji
+import torch
+from googletrans import Translator
+translator = Translator()
+from transformers import pipeline
+pipe = pipeline("text-classification", model="papluca/xlm-roberta-base-language-detection")
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+tokenizer = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
+model = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
 
 _LINK_RE = re.compile(
     r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
@@ -32,3 +43,25 @@ def filter_relevant_comments(comments: List[str]) -> List[str]:
         if ratio >= TEXT_RATIO_THRESHOLD:
             kept.append(c2)
     return kept
+
+def detect_lang(text_to_classify_direct: str) -> str: # will return "hindi" or "english"
+
+    # Tokenize the input
+    inputs = tokenizer(text_to_classify_direct, return_tensors="pt")
+
+    # Perform inference
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    # Get predicted class (logits)
+    logits = outputs.logits
+
+    hindi_prob = logits[0, 9]
+    english_prob = logits[0, 13]
+    
+    if hindi_prob > english_prob:
+        return "hindi"
+    return "english"
+
+def translate(text: str) -> str: # Romanized Hindi to English
+    return translator.translate(text, src='hi', dest='en').text
